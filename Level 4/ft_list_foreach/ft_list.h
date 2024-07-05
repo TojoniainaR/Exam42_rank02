8fe1496f6a51b53168e4977ee1369c1b9eecea5d
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r4s13.42antananarivo.mg>
2024-07-05 11:47:58 +0300

    Level 4

--- Level 4/ft_list_foreach/ft_list.h
+++ Level 4/ft_list_foreach/ft_list.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: torandri <torandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 11:45:21 by torandri          #+#    #+#             */
+/*   Updated: 2024/07/05 11:47:23 by torandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_LIST_H
 # define FT_LIST_H
 
 typedef struct s_list
 {
-    struct s_list   *next;
-    void            *data;
-}               t_list;
+	struct s_list	*next;
+	void			*data;
+}				t_list;
 
 #endif
--- Level 4/ft_list_foreach/ft_list_foreach.c
+++ Level 4/ft_list_foreach/ft_list_foreach.c
@@ -6,17 +6,17 @@
 /*   By: torandri <torandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:33:33 by torandri          #+#    #+#             */
-/*   Updated: 2024/07/05 11:41:19 by torandri         ###   ########.fr       */
+/*   Updated: 2024/07/05 11:47:48 by torandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
 
-void    ft_list_foreach(t_list *begin_list, void (*f)(void *))
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
-    while (begin_list)
-    {
-        (*f)(begin_list->data);
-        begin_list = begin_list->next;
-    }
+	while (begin_list)
+	{
+		(*f)(begin_list->data);
+		begin_list = begin_list->next;
+	}
 }
