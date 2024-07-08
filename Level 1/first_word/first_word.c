02df385c72a6d393e77bf711c9f6cff7fe5029c5
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r13s9.42antananarivo.mg>
2024-07-08 14:49:44 +0300

    first_word

--- Level 1/first_word/first_word.c
+++ Level 1/first_word/first_word.c
@@ -19,7 +19,9 @@ int	main(int ac, char *av[])
 	if (ac == 2)
 	{
 		i = 0;
-		while (av[1][i] != ' ' && av[1][i] != '\0')
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while ((av[1][i] != ' ' && av[1][i] != '\t') && av[1][i] != '\0')
 			write(1, &av[1][i++], 1);
 	}
 	write(1, "\n", 1);
