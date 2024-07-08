acdd4d6f5fba2ac769a0c00b4681a6dd072c5e6d
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r13s9.42antananarivo.mg>
2024-07-08 14:49:51 +0300

    alpha_mirror

--- Level 2/alpha_mirror/alpha_mirror.c
+++ Level 2/alpha_mirror/alpha_mirror.c
@@ -18,14 +18,14 @@ int	main(int ac, char *av[])
 
 	if (ac == 2)
 	{
+		i = 0;
 		while (av[1][i] != '\0')
 		{
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 				av[1][i] = 'z' - av[1][i] + 'a';
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 				av[1][i] = 'Z' - av[1][i] + 'A';
-			write(1, &av[1][i], 1);
-			i++;
+			write(1, &av[1][i++], 1);
 		}
 	}
 	write(1, "\n", 1);
