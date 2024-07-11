ea8ee0c40f59a88ee3159565a4b8dec81657a6e5
Tojoniaina Fenosoa Gian Elias Randriamanana <torandri@c1r13s9.42antananarivo.mg>
2024-07-11 13:34:12 +0300

    union

--- Level 2/union/union.c
+++ Level 2/union/union.c
@@ -6,8 +6,35 @@
 /*   By: torandri <torandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 15:49:28 by torandri          #+#    #+#             */
-/*   Updated: 2024/07/05 20:52:30 by torandri         ###   ########.fr       */
+/*   Updated: 2024/07/11 13:33:30 by torandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int	check_double(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_union(char *str1, char *str2)
+{
+	return ;
+}
+
+int	main(int ac, char *av[])
+{
+	if (ac == 2)
+		ft_union(av[1], av[2]);
+	write (1, "\n", 1);
+	return (0);
+}
